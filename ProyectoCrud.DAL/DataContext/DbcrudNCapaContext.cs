81bf00d8ea0f46f5af007d1c7bf2c9982ceb2ed3
Brandon De La Cruz <brandon.delacruz@coppel.com>
2025-02-11 17:35:39 -0700

    Implementacion de capa datos (DAL-data access layer) y negocio (BLL-buisness logic layer)

---
+++ ProyectoCrud.DAL/DataContext/DbcrudNCapaContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ProyectoCrud.Models;
+
+namespace ProyectoCrud.Datos.DataContext;
+
+public partial class DbcrudNCapaContext : DbContext
+{
+    public DbcrudNCapaContext()
+    {
+    }
+
+    public DbcrudNCapaContext(DbContextOptions<DbcrudNCapaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contacto> Contactos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contacto>(entity =>
+        {
+            entity.HasKey(e => e.IdContacto).HasName("PK__CONTACTO__A4D6BBFA4447DB11");
+
+            entity.ToTable("CONTACTO");
+
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ProyectoCrud.DAL/Repositorio/ContactoRepository.cs
@@ -0,0 +1,51 @@
+﻿using ProyectoCrud.Datos.DataContext;
+using ProyectoCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCrud.Datos.Repositorio
+{
+    public class ContactoRepository : IGenericRepository<Contacto>  //HEREDA TODOS LOS METODOS DEL CONTRATO CON LA CLASE GENERICA
+    {
+        private readonly DbcrudNCapaContext _dbcontext;
+        public ContactoRepository(DbcrudNCapaContext context)
+        {
+            _dbcontext = context;
+        }
+        public async Task<bool> Actualizar(Contacto modelo)
+        {
+            _dbcontext.Contactos.Update(modelo);
+            await _dbcontext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            Contacto modelo = _dbcontext.Contactos.First(c=>c.IdContacto == id);
+            _dbcontext.Contactos.Remove(modelo);
+            await _dbcontext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Insertar(Contacto modelo)
+        {
+            _dbcontext.Contactos.Add(modelo);
+            await _dbcontext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Contacto> Obtener(int id)
+        {
+            return await _dbcontext.Contactos.FindAsync(id);
+        }
+
+        public async Task<IQueryable<Contacto>> ObtenerTodos()
+        {
+            IQueryable < Contacto > queryContactoSQL = _dbcontext.Contactos;
+            return queryContactoSQL;
+        }
+    }
+}
